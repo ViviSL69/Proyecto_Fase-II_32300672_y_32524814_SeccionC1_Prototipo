adca43bcaf58dd5fd7a2f7ce98fcaaa5de9009ce
ViviSL69 <sofiascaniov@gmail.com>
2026-03-13 20:52:45 -0400

    Update productos.js

--- proyecto2/js/productos.js
+++ proyecto2/js/productos.js
@@ -9,35 +9,35 @@ const productos =[
         id: 2,
         nombre: "Empanada de Queso",
         precio: 2.50,
-        imagen: "./imagenes/empanada.png"
+        imagen: "imagenes/empanada.png"
 
     },
      {
         id: 3,
         nombre: "Jugo Yuky-Pak",
         precio: 3.00,
-        imagen: "./imagenes/yukipak.png"
+        imagen: "imagenes/yukipak.png"
 
     },
      {
         id: 4,
         nombre: "Sandwich",
         precio: 4.50,
-        imagen: "./imagenes/sandwich.png"
+        imagen: "imagenes/sandwich.png"
 
     },
      {
         id: 5,
         nombre: "Brownie",
         precio: 1.00,
-        imagen: "./imagenes/brownie.png"
+        imagen: "imagenes/brownie.png"
 
     },
      {
         id: 6,
         nombre: "Coca-Cola",
         precio: 3.00,
-        imagen: "./imagenes/cocacola.png"
+        imagen: "imagenes/cocacola.png"
 
     }, 
     
